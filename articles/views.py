677f8246ee03c35d69cbf923ac071f658552d462
Equinox-13 <quaid.johar33@gmail.com>
2020-01-05 20:37:29 +0530

    Created articles app and created template to display it using bootstrap

---
+++ articles/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Article
+
+class ArticleListView(ListView):
+	model = Article
+	template_name = 'article_list.html'
--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     # Local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
+    'articles.apps.ArticlesConfig',
      
      # 3rd Party
     'crispy_forms',
@@ -112,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
