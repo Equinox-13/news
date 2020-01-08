8b5503eb07fefd42f73aad792d18dbd5de21ecff
Equinox-13 <quaid.johar33@gmail.com>
2020-01-08 09:34:29 +0530

    Added create article page and 'New' menu in navigation bar

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 from .models import Article
 
@@ -24,3 +24,9 @@ class ArticleDeleteView(DeleteView):
 	model = Article
 	template_name = 'article_delete.html'
 	success_url = reverse_lazy('article_list')
+
+
+class ArticleCreateView(CreateView):
+	model = Article
+	fields = ('title','body', 'author',)
+	template_name = 'article_new.html'
