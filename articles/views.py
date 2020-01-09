a2acc6f6feb8f170f3ebb5f262a5ada386771de1
Equinox-13 <quaid.johar33@gmail.com>
2020-01-09 09:52:55 +0530

    Added dispatch() method to authenticate user before updation or deletion

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
@@ -23,12 +24,32 @@ class ArticleUpdateView(LoginRequiredMixin,UpdateView):
 	template_name = 'article_edit.html'
 	login_url = 'login'
 
+	def dispatch(self, request, *args, **kwargs):
+		"""
+		checks wheather the updation is done 
+		by the same user who has created it
+		"""
+		obj = self.get_object()
+		if obj.author != self.request.user:
+			raise PermissionDenied
+		return super().dispatch(request, *args, **kwargs)
+
 
 class ArticleDeleteView(LoginRequiredMixin,DeleteView):
 	model = Article
 	template_name = 'article_delete.html'
 	success_url = reverse_lazy('article_list')
 	login_url = 'login'
+
+	def dispatch(self, request, *args, **kwargs):
+		"""
+		checks wheather the deletion is done 
+		by the same user who has created it
+		"""
+		obj = self.get_object()
+		if obj.author != self.request.user:
+			raise PermissionDenied
+		return super().dispatch(request, *args, **kwargs)
 
 
 class ArticleCreateView(LoginRequiredMixin,CreateView):
