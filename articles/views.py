6c03b40dcf33f0c030b1e4c9b23c803f22e90999
Equinox-13 <quaid.johar33@gmail.com>
2020-01-09 09:41:44 +0530

    sets author name based on logged in user on creation, added mixin to check user authentication before opening any page

--- articles/views.py
+++ articles/views.py
@@ -1,32 +1,42 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 from .models import Article
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin,ListView):
 	model = Article
 	template_name = 'article_list.html'
+	login_url = 'login'
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin,DetailView):
 	model = Article
 	template_name = 'article_detail.html'
+	login_url = 'login'
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin,UpdateView):
 	model = Article
 	fields = ('title','body',)
 	template_name = 'article_edit.html'
+	login_url = 'login'
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin,DeleteView):
 	model = Article
 	template_name = 'article_delete.html'
 	success_url = reverse_lazy('article_list')
+	login_url = 'login'
 
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin,CreateView):
 	model = Article
-	fields = ('title','body', 'author',)
+	fields = ('title','body',)
 	template_name = 'article_new.html'
+	login_url = 'login' # redirects to login page if user not signed in
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
