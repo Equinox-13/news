af95dc5deac30598d6ef5af174cd3b83c7b090ac
Equinox-13 <quaid.johar33@gmail.com>
2020-01-11 12:43:12 +0530

    Added comments in article list page

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Article, Comment
 
-admin.site.register(Article)
+class CommentInline(admin.StackedInline):
+	# replace StackdInline to TabularInline for single line display in admin page
+	model = Comment
+
+
+class ArticleAdmin(admin.ModelAdmin):
+	inlines = [CommentInline,]
+
+admin.site.register(Article,ArticleAdmin)
 admin.site.register(Comment)
--- articles/models.py
+++ articles/models.py
@@ -17,7 +17,7 @@ class Article(models.Model):
 
 
 class Comment(models.Model):
-	article = models.ForeignKey(Article, on_delete=models.CASCADE)
+	article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
 	comment = models.CharField(max_length=140)
 	author = models.ForeignKey(get_user_model(),on_delete=models.CASCADE,)
 
